b283d1009e5c18a4a3a170638f792948c5e0eb90
Loge <38889912+yw9594@users.noreply.github.com>
2021-10-06 21:37:57 +0900

    Update action02.js

--- action02.js
+++ action02.js
@@ -10,8 +10,8 @@ const octokit = new Octokit({
 
 
 octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/labels', {
-    owner: 'EntTangunGHEC',
-    repo: 'KakaoSeminar',
+    owner: 'yw9594',
+    repo: 'study-guthub-action',
     issue_number: core.getInput('issue_number'),
     labels: [
       'bug'
